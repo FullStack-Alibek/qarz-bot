ebf19d44b0e913704ffbb67955d4f0e82e47e58a
Alibek Choriyev <info@gmail.com>
2026-02-23 20:19:11 +0500

    production debloy

--- core/database.js
+++ core/database.js
@@ -1,13 +1,10 @@
 const { Pool } = require("pg")
-const { isProd } = require("./env")
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    user: process.env.DB_USER,
-    password: String(process.env.DB_PASSWORD),
-    database: process.env.DB_NAME,
-    ssl: isProd ? { rejectUnauthorized: false } : false
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
 
 pool.on("connect", () => console.log("Data bazaga ulandi"))
